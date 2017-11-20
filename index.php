4261c30cf0f603a5f3152231b15b2cfaf6e3a0c6
LeninEDU <32774567+LeninEDU@users.noreply.github.com>
2017-11-20 13:53:31 +0100

    plugin name

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<?php ?>
+<?php
+   /*
+   Plugin Name: oke plugin
+   Plugin URI:
+   Description: a plugin
+   Version: 1.2
+   Author: oke
+   Author URI: http://mrtotallyawesome.com
+   License: GPL2
+   */
+?>
