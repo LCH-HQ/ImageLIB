8bbe6758c27ea971c9fb90f65379e71d6fd4f94c
jimkraan <33829789+jimkraan@users.noreply.github.com>
2018-01-08 15:43:14 +0100

    CPT fix, permalink fix, archive fix

--- Inc/Pages/Admin.php
+++ Inc/Pages/Admin.php
@@ -16,6 +16,7 @@ use Inc\Api\Callbacks\AdminCallbacks;
 
 class Admin extends BaseController
 {
+
 	public $instellingen;
 
 	public $callbacks;
@@ -23,6 +24,8 @@ class Admin extends BaseController
 	public $paginas = array();
 
 	public $subpaginas = array();
+
+	public $posts;
 
 	// Verwerk de pagina, subpagina's en content en push deze naar de back-end
 	public function registreren() 
@@ -42,7 +45,9 @@ class Admin extends BaseController
 		$this->instellingen->paginasToevoegen( $this->paginas )->metSubPagina( 'Dashboard' )->subPaginasToevoegen( $this->subpaginas)->registreren();
 
 		// Activeer submenu voor Ruimtes
-		add_action('admin_menu', array( $this, 'activeerRuimteCPT') ); 
+		add_action('admin_menu', array( $this, 'genereerRuimteCPT') );
+		add_action( 'init', array( $this, 'genereerRuimteCPT') );
+ 
 	}
 
 	// Genereer de pagina van SpaceBooker voor de back-end
@@ -88,43 +93,127 @@ class Admin extends BaseController
 	 * Genereer CPT voor de ruimtes
 	 */
 
-	public function activeerRuimteCPT() {
+	public function genereerRuimteCPT() {
 
 		$enkelvoud = 'ruimte';
 		$meervoud = 'ruimtes';
 
-		register_post_type( 'ruimte',
-		    array(
-		            'labels' => array(
-		                    'name' => __( 'Ruimtes' ),
-		                    'singular_name' => __( 'Ruimte' ),
-		                    'add_new_item' => ( "Nieuwe $enkelvoud toevoegen" ),
-		                    'edit_item' => ( "Bewerk $enkelvoud" ),
-		                    'new_item' => ( "Nieuwe $enkelvoud" ),
-		                    'view_item' => ( "Bekijk $enkelvoud" ),
-		                    'view_items' => ( "Bekijk $meervoud" ),
-		                    'search_items' => ( "Zoeken naar $meervoud" ),
-		                    'not_found' => ( "Geen $meervoud gevonden" ),
-		                    'not_found_in_trash' => ( "Geen $meervoud gevonden in de prullenbak" ),
-		                    'all_items' => ( "Alle $meervoud" ),
-		                    'archives' => ( "Archief voor $meervoud" ),
-		                    'attributes' => ( "Attributen voor $meervoud"),
-		                    'insert_into_item' => ( "Voeg in $enkelvoud" ),
-		                    'uploaded_to_this_item' => ( "Geüpload naar deze $enkelvoud" )
-		            ),
-		    'public' => true,
-		    'publicly_queryable' => true,
-		    'has_archive' => true,
-		    'menu-position' => 5,
-		    'capability_type' => 'post',
-		    'show_in_menu' => 'spacebooker',
-		    'map_meta_cap' => true,
-		    'rewrite' => array( 'slug' => 'ruimtes', 'with_front' => true, 'pages' => true, 'feeds' => true),
-		    'supports' => array('title', 'custom-fields', 'page-attributes', 'post-formats')
-		    )
-		);
+	        $labels = array(
+	                'name' => _x( 'Ruimtes', 'De ruimtes' ),
+	                'singular_name' => _x( 'Ruimte', 'De ruimte' ),
+	                'add_new_item' => _x( "Nieuwe $enkelvoud toevoegen", 'ruimte' ),
+	                'edit_item' => __( "Bewerk $enkelvoud" ),
+	                'new_item' => __( "Nieuwe $enkelvoud" ),
+	                'view_item' => __( "Bekijk $enkelvoud" ),
+	                'view_items' => __( "Bekijk $meervoud" ),
+	                'search_items' => __( "Zoeken naar $meervoud" ),
+	                'not_found' => __( "Geen $meervoud gevonden" ),
+	                'not_found_in_trash' => ( "Geen $meervoud gevonden in de prullenbak" ),
+	                'all_items' => __( "Ruimtes" ),
+	                'archives' => __( "Archief voor $meervoud" ),
+	                'attributes' => __( "Attributen voor $meervoud"),
+	                'insert_into_item' => __( "Voeg in $enkelvoud" ),
+	                'uploaded_to_this_item' => __( "Geüpload naar deze $enkelvoud" ),
+	                'menu_name' => __('Ruimtes')
+	        );
+	    	$args = array(
+			    'labels' => $labels,
+			    'public' => true,
+			    'publicly_queryable' => true,
+			    'show_ui' => true,
+			  	'query_var' => true,
+			    'menu-position' => 5,
+			    'capability_type' => 'post',
+			    'show_in_menu' => 'spacebooker',
+			    'has_archive' => true,
+			    'map_meta_cap' => true,
+			    'rewrite' => array( 'slug' => _x( 'ruimte', 'ruimtes') ),
+			    'supports' => array('title', 'custom-fields', 'page-attributes', 'post-formats')
+		    );
 
-		add_submenu_page('spacebooker', 'Ruimtes beheren', 'Ruimtes', 'manage_options', 'edit.php?post_type=ruimte');
+		register_post_type('ruimte_posts', $args);
+
+		flush_rewrite_rules();
+
+		add_shortcode( 'print_ruimtes', array( $this, 'activeerRuimteCPT' ) );
+		add_shortcode( 'custom-form', array($this, 'printRuimteFormulier') );
+	}
+
+	public function activeerRuimteCPT() {
+		$print_alle_posts = get_posts( array(
+			'post_status' => 'publish',
+			'posts_per_page' => 10,
+			'post_parent' => null
+		) );
+
+		if ( $print_alle_posts ) {
+			foreach ( $print_alle_posts as $post ) {
+				setup_postdata( $post );
+				the_title();
+				the_meta();
+			}
+			wp_reset_postdata();
+		}
+	}
+
+	public function printRuimteFormulier() {
+		?>
+		<form id="custom-post-type" name="custom-post-type" method="post" action="">
+		 
+		<p><label for="title">Post Title</label><br />
+		 
+		<input type="text" id="title" value="" tabindex="1" size="20" name="title" />
+		 
+		</p>
+		 
+		<p><?php wp_dropdown_categories( 'show_option_none=Category&tab_index=4&taxonomy=category' ); ?></p>
+		
+		 
+		<p align="right"><input type="submit" value="Publish" tabindex="6" id="submit" name="submit" /></p>
+		 
+		<input type="hidden" name="post-type" id="post-type" value="ruimte_posts" />
+		 
+		<input type="hidden" name="action" value="ruimte_posts" />
+		 
+		<?php wp_nonce_field( 'name_of_my_action','name_of_nonce_field' ); ?>
+		 
+		</form>
+		<?php
+
+		if($_POST){
+			$this->slaRuimteOp();
+		}
+	}
+
+	public function slaRuimteOp() {
+		if ( empty($_POST) || !wp_verify_nonce($_POST['name_of_nonce_field'],'name_of_my_action') )
+		{
+		print 'Sorry, your nonce did not verify.';
+		exit;
+		 
+		}else{
+		 
+		// Do some minor form validation to make sure there is content
+		if (isset ($_POST['title'])) {
+		$title =  $_POST['title'];
+		} else {
+		echo 'Please enter a title';
+		exit;
+		}
+		 
+		// Add the content of the form to $post as an array
+		$post = array(
+		'post_title' => wp_strip_all_tags( $title ),
+		'post_category' => $_POST['cat'],  // Usable for custom taxonomies too
+		'post_status' => 'publish',            // Choose: publish, preview, future, etc.
+		'post_type' => $_POST['post-type']  // Use a custom post type if you want to
+		);
+		wp_insert_post($post);  // http://codex.wordpress.org/Function_Reference/wp_insert_post
+		 
+		$location = home_url(); // redirect location, should be login page
+		 
+		echo "<meta http-equiv='refresh' content='0;url=$location' />"; exit;
+		} // end IF
 	}
 
 	/*
