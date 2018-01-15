f84390d3b69916fc9f193116f03134fa6f9cee8f
LeninCH1 <lenin_249@hotmail.com>
2018-01-15 12:36:07 +0100

    animatie
    voeg onclick="toonMeerInfo();" functie om te animeren

--- spacebooker-frontend/js/animatie.js
+++ spacebooker-frontend/js/animatie.js
@@ -1,79 +1,17 @@
 //$(document).ready(function(){
 
+function toonMeerInfo(){
 
-function Toon() {
-    var x = document.getElementById("Ruimtes");
-    if (x.style.height === "42px") {
-        x.style.height = "100px";
-    } else {
-        x.style.height = "42px";
-
-    }
-  }
+$(function(){
+    $(".ruimte-row").click(function(){$(this).toggleClass("active")})
+})
 
 
-
-  function Toon1() {
-      var x = document.getElementById("Ruimtes1");
-      if (x.style.height === "42px") {
-          x.style.height = "100px";
-      } else {
-          x.style.height = "42px";
-
-      }
-    }
-
-
-
-    function Toon2() {
-        var x = document.getElementById("Ruimtes2");
-        if (x.style.height === "42px") {
-            x.style.height = "100px";
-        } else {
-            x.style.height = "42px";
-        }
-
-      }
-
-
-
-      function Toon3() {
-          var x = document.getElementById("Ruimtes3");
-          if (x.style.height === "42px") {
-              x.style.height = "100px";
-          } else {
-              x.style.height = "42px";
-
-          }
-        }
-
-
-
-          function Toon4() {
-              var x = document.getElementById("Ruimtes4");
-              if (x.style.height === "42px") {
-                  x.style.height = "100px";
-              } else {
-                  x.style.height = "42px";
-
-              }
-            }
+}
 
 
 
 
-            // function ToonX() {
-            //     var x = document.getElementById("Ruimtes");
-            //     var x = document.getElementById("Ruimtes1");
-            //     var x = document.getElementById("Ruimtes2");
-            //     var x = document.getElementById("Ruimtes3");
-            //     var x = document.getElementById("Ruimtes4");
-            //
-            //     if (x.style.height === "42px") {
-            //         x.style.height = "100px";
-            //     }
-            //
-            //     else {
-            //         x.style.height = "42px";
-            //     }
-            //   }
+// $(function(){
+//     $(".ruimte-row").click(function(){$(this).toggleClass("active")})
+// })
